10583ca4f589fb9b3f0cc71116851f26fd772d50
milliebase <betsyalva@milliebase.se>
2019-11-11 17:30:16 +0100

    Changed linkback and vh

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 
-const linkBack = document.getElementsByClassName('spaceTop')[0];
+const linkBack = document.getElementsByClassName('origin')[0];
 const text = document.getElementById('origin');
 const button = document.getElementById('more');
 
 
 
-text.style.height ="75vh";
+text.style.height ="60vh";
 text.style.overflow = "hidden"
 
 
 button.onclick = function (){
-    if (text.style.height === "75vh"){  
+    if (text.style.height === "60vh"){  
      text.style.height = "auto";
-     button.innerHTML = "Less";  
+     button.innerHTML = "Read less...";  
      
 
     }else if(text.style.height === "auto"){
-        text.style.height = "75vh";
-        button.innerHTML = "More";
+        text.style.height = "60vh";
+        button.innerHTML = "Read more...";
         linkBack.scrollIntoView();
         }
     }
